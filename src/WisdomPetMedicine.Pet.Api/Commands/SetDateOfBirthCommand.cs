17e1c5ca6d0a8e8c9c37dd2d18d3e5df371ea943
Onur Ozguzel <ool@emakina.com>
2022-06-11 19:37:31 +0300

    added another query controller

---
+++ src/WisdomPetMedicine.Pet.Api/Commands/SetDateOfBirthCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.Pet.Api.Commands
+{
+    public class SetDateOfBirthCommand
+    {
+        public Guid Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ src/WisdomPetMedicine.Pet.Api/Controllers/PetQueryController.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.Pet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetQueryController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public PetQueryController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string query = @"select p.name_value as Name,
+p.breed_value as Breed,
+sex = case p.sexofpet_value when 0 then 'Male' when 1 then 'Female' end,
+p.color_value as color,
+p.dateofbirth_value as DateOfBirth,
+p.species_value as Species
+from pets p";
+
+            using var connection = new SqlConnection(configuration.GetConnectionString("Pet"));
+            var pets = (await connection.QueryAsync(query)).ToList();
+            return Ok(pets);
+        }
+    }
+}
