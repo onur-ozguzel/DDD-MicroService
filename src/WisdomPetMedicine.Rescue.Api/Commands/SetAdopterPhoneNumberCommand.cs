b1f16019512b5c7e3f92d1da25e7a3a8c6e0a1dd
Onur Ozguzel <ool@emakina.com>
2022-06-11 19:28:50 +0300

    added new query service

---
+++ src/WisdomPetMedicine.Rescue.Api/Commands/SetAdopterPhoneNumberCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.Rescue.Api.Commands
+{
+    public class SetAdopterPhoneNumberCommand
+    {
+        public Guid Id { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
