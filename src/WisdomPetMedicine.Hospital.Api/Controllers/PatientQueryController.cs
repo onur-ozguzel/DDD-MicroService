69e37775e85abb792cb0ba674adb5ed914a121ff
Onur Ozguzel <ool@emakina.com>
2022-06-12 21:50:14 +0300

    added api gateway

---
+++ src/WisdomPetMedicine.Hospital.Api/Controllers/PatientQueryController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.Hospital.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientQueryController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public PatientQueryController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = @"SELECT pm.*, p.BloodType, p.Weight, p.Status, p.UpdatedOn
+                            FROM PatientsMetadata pm 
+                            JOIN Patients p
+                            ON pm.Id = p.Id";
+            using var connection = new SqlConnection(configuration.GetConnectionString("Hospital"));
+            var orderDetail = (await connection.QueryAsync(sql)).ToList();
+            return Ok(orderDetail);
+        }
+    }
+}
